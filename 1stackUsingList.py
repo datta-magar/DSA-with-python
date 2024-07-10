64b1d2661988d0f13321f0312097ab59e960f0b8
Magar <dattatraya.magar@capgemini.com>
2024-07-10 11:22:14 +0530

    code number 1

--- 1stackUsingList.py
+++ 1stackUsingList.py
@@ -1,1 +1,36 @@
-print("test")
+#implement stack using list
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def is_empty(self):
+        return len(self.stack) == 0
+    
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("pop from empty stack")
+        return self.stack.pop()
+
+    def peak(self):
+        if self.is_empty():
+            raise ImportError("peak form empty stack")
+        return self.stack[-1]
+        
+    def size(self):
+        return len(self.stack)
+    
+#test the Stack 
+stack = Stack()
+stack.push(11)
+stack.push(22)
+print("size ",stack.size())
+print("peak ", stack.peak())
+print("popped ",stack.pop())
+print("is empty ", stack.is_empty())
+print("popped ", stack.pop())
+print("is empty ", stack.is_empty())
+        
+    
