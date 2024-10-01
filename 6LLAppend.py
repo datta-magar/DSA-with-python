dd3a3388bcd17932de88c60d96854d9e7c411bac
Magar <dattatraya.magar@capgemini.com>
2024-10-01 15:34:48 +0530

    append LL

---
+++ 6LLAppend.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.length += 1
+    
+    def empty(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+my_ll = LinkedList(1)
+my_ll.append(2)
+my_ll.append(3)
+
+my_ll.print_list()
