618913acf09dcd960f2d4796516740f9950dd3af
Magar <dattatraya.magar@capgemini.com>
2024-10-01 17:09:19 +0530

    pop last node LL

---
+++ 7LLPop.py
@@ -0,0 +1,175 @@
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.next = None
+
+# class LinkedList:
+#     def __init__(self, value):
+#         new_node = Node(value)
+#         self.head = new_node
+#         self.tail = new_node
+#         self.length = 1
+
+#     def append(self, value):
+#         new_node = Node(value)
+#         if self.head is None:
+#             self.head = new_node
+#             self.tail = new_node
+#         else:
+#             self.tail.next = new_node
+#             self.tail = new_node
+#         self.length += 1
+
+#     def print_list(self):
+#         temp = self.head
+#         while temp is not None:
+#             print(temp.value)
+#             temp = temp.next
+
+#     def pop(self):
+#         if self.head is None:
+#             return None
+#         if self.length == 1:
+#             temp = self.head
+#             self.head = None
+#             self.tail = None
+#             self.length = 0
+#             return temp
+#         else:
+#             temp = self.head
+#             while temp.next.next is not None:
+#                 temp = temp.next
+                
+#             self.tail = temp
+#             self.tail.next = None
+#             self.length -= 1
+#             return temp
+
+# my_ll = LinkedList(1)
+# my_ll.append(2)
+# my_ll.append(3)
+
+# my_ll.print_list()
+
+
+# # my_ll.pop()
+# popped_node = my_ll.pop()
+# print('\npopped node',popped_node.value)
+# print('\n')
+
+# my_ll.print_list()
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        values = []
+        temp = self.head
+        while temp is not None:
+            values.append(str(temp.value))
+            temp = temp.next
+        values.append("None")
+        print(" -> ".join(values))
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.head is None:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next is not None:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+
+
+ 
+##########################################################   
+##   Test code below will print output to "User logs"   ##
+##########################################################
+
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
+
+print("\n----- Test: Pop on linked list with one node -----\n")
+linked_list = LinkedList(1)
+linked_list.print_list()
+popped_node = linked_list.pop()
+check(1, popped_node.value, "Value of popped node:")
+check(None, linked_list.head, "Head of linked list:")
+check(None, linked_list.tail, "Tail of linked list:")
+check(0, linked_list.length, "Length of linked list:")
+
+print("\n----- Test: Pop on linked list with multiple nodes -----\n")
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.print_list()
+popped_node = linked_list.pop()
+check(3, popped_node.value, "Value of popped node:")
+check(1, linked_list.head.value, "Head of linked list:")
+check(2, linked_list.tail.value, "Tail of linked list:")
+check(2, linked_list.length, "Length of linked list:")
+
+print("\n----- Test: Pop on empty linked list -----\n")
+linked_list = LinkedList(1)
+linked_list.head = None
+linked_list.tail = None
+linked_list.length = 0
+popped_node = linked_list.pop()
+check(None, popped_node, "Popped node from empty linked list:")
+check(None, linked_list.head, "Head of linked list:")
+check(None, linked_list.tail, "Tail of linked list:")
+check(0, linked_list.length, "Length of linked list:")
+
+print("\n----- Test: Pop all -----\n")
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.print_list()
+popped_node = linked_list.pop()
+check(2, popped_node.value, "Value of popped node (first pop):")
+check(1, linked_list.head.value, "Head of linked list (after first pop):")
+check(1, linked_list.tail.value, "Tail of linked list (after first pop):")
+check(1, linked_list.length, "Length of linked list (after first pop):")
+popped_node = linked_list.pop()
+check(1, popped_node.value, "Value of popped node (second pop):")
+check(None, linked_list.head, "Head of linked list (after second pop):")
+check(None, linked_list.tail, "Tail of linked list (after second pop):")
+check(0, linked_list.length, "Length of linked list (after second pop):")
+popped_node = linked_list.pop()
+check(None, popped_node, "Popped node from empty linked list (third pop):")
+check(None, linked_list.head, "Head of linked list (after third pop):")
+check(None, linked_list.tail, "Tail of linked list (after third pop):")
+check(0, linked_list.length, "Length of linked list (after third pop):")
+
+
