6e58596d01462289d813471fb07781007b2c04cd
Magar <dattatraya.magar@capgemini.com>
2024-10-03 12:22:34 +0530

    LL pop first

---
+++ 9LLPopFirst.py
@@ -0,0 +1,145 @@
+# class Node:
+#     def __init__(self , value):
+#         self.value = value
+#         self.next = None
+
+# class LinkedList:
+#     def __init__(self, value):
+#         new_node = Node(value)
+#         self.head = new_node
+#         self.tail = new_node
+#         self.length = 1
+
+#     def print_list(self):
+#         temp = self.head
+#         while temp is not None:
+#             print(temp.value)
+#             temp = temp.next
+
+#     def append(self, value):
+#         new_node = Node(value)
+#         self.tail.next = new_node
+#         self.tail = new_node
+#         self.length += 1
+#         return True
+
+#     def prepend(self, value):
+#         new_node = Node(value)
+#         if self.head is None:
+#             self.head = new_node
+#             self.tail = new_node
+#         else:
+#             new_node.next = self.head
+#             self.head = new_node
+#         self.length += 1
+#         return True
+
+#     def pop_first(self):
+#         if self.head is None:
+#             print('The list is already empty')
+#         else:
+#             self.head = self.head.next
+#             self.length -= 1
+
+# my_ll = LinkedList(1)
+# my_ll.append(2)
+# my_ll.prepend(0)
+# my_ll.print_list()
+# print('after pop first ')
+# my_ll.pop_first()
+# my_ll.print_list()
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.head is None: 
+            return None
+        
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        
+        if self.head is None:
+            self.tail = None
+        return temp
+
+
+
+
+my_linked_list = LinkedList(2)
+my_linked_list.append(1)
+
+
+# (2) Items - Returns 2 Node
+print(my_linked_list.pop_first().value)
+# (1) Item -  Returns 1 Node
+print(my_linked_list.pop_first().value)
+# (0) Items - Returns None
+print(my_linked_list.pop_first())
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    2
+    1
+    None
+
+"""
