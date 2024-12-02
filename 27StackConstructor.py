c9b50f86dc66d477626599d905eede52932b8699
D <datta@Ds-MacBook-Air.local>
2024-12-02 09:30:18 +0530

    Stack constructor complete with pop push

--- 27StackConstructor.py
+++ 27StackConstructor.py
@@ -15,5 +15,27 @@ class Stack:
             print(temp.value)
             temp = temp.next
 
+    def push(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
 my_stack = Stack(1)
+print(my_stack.pop().value)
+my_stack.push(1)
+my_stack.push(2)
 my_stack.print()
